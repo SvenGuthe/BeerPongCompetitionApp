4ee0d3e1f1810f32cecc0f99b9959225e31d6795
SvenGuthe <sven_guthe@web.de>
2021-01-02 02:35:59 +0100

    Add Player Role initialy

--- Backend/src/main/java/de/guthe/sven/beerpong/tournamentplaner/controller/authentication/RegisterController.java
+++ Backend/src/main/java/de/guthe/sven/beerpong/tournamentplaner/controller/authentication/RegisterController.java
@@ -1,8 +1,11 @@
 package de.guthe.sven.beerpong.tournamentplaner.controller.authentication;
 
+import de.guthe.sven.beerpong.tournamentplaner.datatype.SecurityRole;
 import de.guthe.sven.beerpong.tournamentplaner.model.authentication.ConfirmationToken;
+import de.guthe.sven.beerpong.tournamentplaner.model.authentication.Role;
 import de.guthe.sven.beerpong.tournamentplaner.model.authentication.User;
 import de.guthe.sven.beerpong.tournamentplaner.repository.authentication.ConfirmationTokenRepository;
+import de.guthe.sven.beerpong.tournamentplaner.repository.authentication.RoleRepository;
 import de.guthe.sven.beerpong.tournamentplaner.repository.authentication.UserRepository;
 import de.guthe.sven.beerpong.tournamentplaner.service.EmailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +20,9 @@ public class RegisterController {
     private UserRepository userRepository;
 
     @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
     private ConfirmationTokenRepository confirmationTokenRepository;
 
     @Autowired
@@ -28,6 +34,8 @@ public class RegisterController {
         if (checkUser != null) {
             return checkUser;
         } else {
+            Role playerRole = roleRepository.findByName(SecurityRole.ROLE_PLAYER.toString());
+            user.addRole(playerRole);
             userRepository.save(user);
 
             ConfirmationToken confirmationToken = new ConfirmationToken(user);
