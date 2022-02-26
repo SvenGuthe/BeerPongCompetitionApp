c27751ca1f63f7ccb28e2911c7a406bff6084eb7
SvenGuthe <sven_guthe@web.de>
2022-02-26 16:50:59 +0100

    Initialize Frontend TypeScript React

---
+++ Frontend/src/index.tsx
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={ <App /> } />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
